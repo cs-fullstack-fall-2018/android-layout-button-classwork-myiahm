03b047ac61c898f71f2de955996707556da36ff4
Christopher Pace <Christopher_pace@ymail.com>
2018-11-12 13:00:21 -0600

    check in

---
+++ app/src/main/java/com/example/student/myapplication/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.student.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    String userInput = (EditText) findViewById(R.id.editText);
+    Button button = (Button) findViewById(R.id.button);
+   String textView = (TextView) findViewById(R.id.textView);
+   View.OnClickListener ourOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            numTimesClicked = numTimesClicked + 1;
+            String result = "\nClicked " + numTimesClicked + " times";
+            textView.append(result);
+
+        }
+    };
+        button.setOnClickListener(ourOnClickListener);
+}
+}
